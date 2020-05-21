9a3f70ec734da424942b64acf46ecec10f3e1d00
Glitch (hello-express) <none>
2020-05-21 04:48:20 +0000

    🐵🌋 Checkpoint ./server.js:562109/3095 ./package.json:562109/1313

--- server.js
+++ server.js
@@ -1,34 +1,19 @@
-// server.js
-// where your node app starts
+const target = "https://www.bitmex.com";
+const origin = "https://observablehq.com";
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const time = Date.now();
+let served = 0;
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+require("http-proxy")
+  .createServer({
+    changeOrigin: true, 
+    target,
+    headers: { target }
+  })
+ .on("proxyReq", (proxyRes, req, res) => {
+    proxyRes.headers["access-control-allow-origin"] = "*";
+  })
+  .on("proxyRes", (proxyRes, req, res) => {
+    proxyRes.headers["access-control-allow-origin"] = "*";
+  })
+  .listen(process.env.PORT);
